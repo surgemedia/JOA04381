5f47a87f9ba17e5b005f63839b46b583689dbe7c
Alex <alex.king@surgemedia.com.au>
2015-02-06 11:11:50 +1000

    Update - Custom Book

--- enrollment/content-module-meta.php
+++ enrollment/content-module-meta.php
@@ -3,7 +3,11 @@
 				<p><?php the_field('video',$GLOBALS['current_course_obj']); ?></p>
 				</li>
 				<li><span class="dashicons dashicons-book-alt"></span>
-				<p><a href="<?php get_permalink( get_field('text_book', $GLOBALS['current_course_obj'])[0]->ID); ?>">Buy the book for this lesson</a> </p>
+				<?php 
+				$book_id =  get_field('text_book', $GLOBALS['current_course_obj']);
+				$book_id = $book_id[0]->ID
+				 ?>
+				<p><a href="<?php get_permalink($book_id[0]->ID); ?>">Buy the book for this lesson</a> </p>
 				</li>
 				<li><span class="dashicons dashicons-awards"></span>
 				<p><?php the_field('text', $GLOBALS['current_course_obj']); ?></p>
