40c0be31dd3bedcc8a68a3da6a80b6fd5c9ef249
Alex <alex.king@surgemedia.com.au>
2015-02-06 11:06:50 +1000

    Update - Get Course Book

--- enrollment/content-module-meta.php
+++ enrollment/content-module-meta.php
@@ -3,7 +3,7 @@
 				<p><?php the_field('video',$GLOBALS['current_course_obj']); ?></p>
 				</li>
 				<li><span class="dashicons dashicons-book-alt"></span>
-				<p><?php the_field('text_book', $GLOBALS['current_course_obj']); ?></p>
+				<p><a href="<?php get_permalink( get_field('text_book', $GLOBALS['current_course_obj'])[0]->ID); ?>">Buy the book for this lesson</a> </p>
 				</li>
 				<li><span class="dashicons dashicons-awards"></span>
 				<p><?php the_field('text', $GLOBALS['current_course_obj']); ?></p>
@@ -12,3 +12,7 @@
 				<p><?php the_field('forum', $GLOBALS['current_course_obj']); ?></p>
 				</li>
 			</ul>
+			<?php
+			//debug(get_field('text_book', $GLOBALS['current_course_obj'])[0]->ID);
+
+			?>
