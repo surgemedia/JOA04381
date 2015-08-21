b06657459c50b952b1ff2882da9886e3d217da57
Alex <surge@aegirs-iMac.local>
2015-08-21 10:30:38 +1000

    New Updates

--- page-modules.php
+++ page-modules.php
@@ -48,7 +48,7 @@ get_header(); ?>
                 </article>
                 <?php 
                 $user_id = getUser();
-                  // Reset All Player Progress
+                 // Reset All Player Progress
                  // xprofile_set_field_data( 'Completed Modules', $user_id, "", $is_required = false );
                  // xprofile_set_field_data( 'Completed Lessons', $user_id, "", $is_required = false );
                  // xprofile_set_field_data( 'Bridge Skill', $user_id, "New Player", $is_required = false );
