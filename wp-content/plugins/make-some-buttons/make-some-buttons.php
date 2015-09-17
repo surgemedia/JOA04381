d9f54b384c98d2cc39341fdf4e2c4b3fe9ba9440
Alex <surge@aegirs-iMac.local>
2015-09-17 14:48:58 +1000

    Remove Plugin

--- wp-content/plugins/make-some-buttons/make-some-buttons.php
+++ wp-content/plugins/make-some-buttons/make-some-buttons.php
@@ -26,53 +26,15 @@ function basic_content_replace_buttons ($text)
 	$sethandstipsfreestart = "";
 	$sethandtipsfreeend = "";
 
-        $lessonhandsstart = "";
-        $lessonhandsend = "";
-	
-	if ( is_user_logged_in() ) {
-	  $myRole = "none";
-	  $role = $wpdb->prefix . 'capabilities';
-	  $current_user->role = array_keys($current_user->$role);
-	  foreach($current_user->role as $role => $Role) {
-	    if ($Role == "administrator" || $Role == "royal") {
-		$myRole = $Role;
-	    }
-	  }
-	  if ($myRole == "none") {		
-		$sethandsstart = "<a class='";
-		$sethandsend = " button icon-play button_filled button_blue' href='/play-bridge-online/'>Upgrade to play lesson hands</a>";
-		$sethandstipsstart = "<a class='";
-		$sethandstipsend = " button icon-play button_filled button_blue' href='/play-bridge-online/'>Upgrade to play tips hands</a>";
-	  } else {
-		$sethandsstart = "<a class='button icon-play button_filled button_blue' href='/play/?game=hands&val=";
-		$sethandsend = "'>PLAY LESSON HANDS</a>";
-		$sethandstipsstart = "<a class='button icon-play button_filled button_blue' href='/play/?game=hands&level=0&val=";
-		$sethandstipsend = "'>PLAY TIPS HANDS</a>";	
-	  }  
-          
-	  $sethandsfreestart = "<a class='button icon-play button_filled button_blue' href='/play/?game=hands&val=";
-	  $sethandsfreeend = "'>PLAY LESSON HANDS</a>";
-	  $sethandstipsfreestart = "<a class='button icon-play button_filled button_blue' href='/play/?game=hands&level=0&val=";
-	  $sethandstipsfreeend = "'>PLAY TIPS HANDS</a>";
-          $lessonhandsstart = "<a class='button icon-play button_filled button_blue' href='/play/?game=coffee&val=";
-          $lessonhandsend = "'>PLAY LESSON HANDS</a>";
-	} else {
-		$sethandsstart = "<a class='";
-		//$sethandsend = "' href='/login/' >login to play lesson hands</a>";
-		$sethandsend = "' href='/wp-login.php' >login to play lesson hands</a>";
-		
-		$sethandsfreestart = "<a class='";
-		//$sethandsend = "' href='/login/' >login to play lesson hands</a>";
-		$sethandsfreeend = "' href='/wp-login.php' >login to play lesson hands</a>";
-		
-		$sethandstipsstart = "<a class='";
-		//$sethandstipsend = "' href='/login/' >login to play tips hands</a>";		
-		$sethandstipsend = "' href='/wp-login.php' >login to play tips hands</a>";
-		
-		$sethandstipsfreestart = "<a class='";		
-		//$sethandstipsfreeend = "' href='/login/' >login to play tips hands</a>";
-		$sethandstipsfreeend = "' href='/wp-login.php' >login to play tips hands</a>";
-	}
+    $lessonhandsstart = "";
+    $lessonhandsend = "";
+
+	$sethandsfreestart = "<a class='button icon-play button_filled button_blue' href='/play/?game=hands&val=";
+	$sethandsfreeend = "'>PLAY LESSON HANDS</a>";
+	$sethandstipsfreestart = "<a class='button icon-play button_filled button_blue' href='/play/?game=hands&level=0&val=";
+	$sethandstipsfreeend = "'>PLAY TIPS HANDS</a>";
+    $lessonhandsstart = "<a class='button icon-play button_filled button_blue' href='/play/?game=coffee&val=";
+    $lessonhandsend = "'>PLAY LESSON HANDS</a>";
 	
 	$text = str_replace("[sethands]",$sethandsstart,$text);
 	$text = str_replace("[/sethands]",$sethandsend,$text);
