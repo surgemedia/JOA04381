cff7ffc1d6a947ae0faca466410f71de55b8c6be
Vani <surge@Desktop-10s-iMac.local>
2015-09-25 15:11:47 +1000

    home page box navigation

--- wp-content/themes/Joan-Butts/template-home.php
+++ wp-content/themes/Joan-Butts/template-home.php
@@ -4,5 +4,6 @@
  */
 ?>
 
-<?php while (have_posts()) : the_post(); ?>
+<?php while (have_posts()) : the_post(); ?> 
+	<?php get_template_part('templates/home','box-nav') ?>
 <?php endwhile; ?>
---
+++ wp-content/themes/Joan-Butts/templates/home-box-nav.php
@@ -0,0 +1,23 @@
+<?php
+	if(have_rows('box_navigation')) {
+?>
+		<section class="box-nav">
+			<ul>
+<?php
+				while(have_rows('box_navigation')) : the_row();
+?>
+			
+					<li class="col-md-2">
+						<a class="link-card" href="<?php the_sub_field('link'); ?>">
+							<span class="link-color <?php echo get_sub_field('color'); ?>"></span>
+							<h3 class="link-text"><?php the_sub_field('link_text'); ?></h3>
+						</a>
+					</li>
+<?php
+				endwhile;
+?>
+			</ul>
+		</section>
+<?php
+	}
+?>
