e9d37795af498c308e233dbe0ed07ddd27e0a178
Vani <surge@Desktop-10s-iMac.local>
2015-10-01 10:02:00 +1000

    Site Video

--- wp-content/themes/Joan-Butts/template-home.php
+++ wp-content/themes/Joan-Butts/template-home.php
@@ -7,4 +7,5 @@
 <?php while (have_posts()) : the_post(); ?> 
 	<?php get_template_part('templates/home','box-nav') ?>
 	<?php get_template_part('templates/home', 'events'); ?>
+	<?php get_template_part('templates/home', 'video'); ?>
 <?php endwhile; ?>
