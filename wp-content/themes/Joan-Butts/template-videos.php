810efdd8e281c88e707356608c28f24cc43fc532
hectornucv <hectornucv@gmail.com>
2015-10-15 11:09:35 +1000

    templates videos

--- wp-content/themes/Joan-Butts/template-videos.php
+++ wp-content/themes/Joan-Butts/template-videos.php
@@ -4,69 +4,39 @@ Template Name: Videos Page
 */
 ?>
 
-							<div class="breadcrumbs">
+							<!-- <div class="breadcrumbs">
 							    <?php if(function_exists('bcn_display'))
 							    {
 							        bcn_display();
 							    }?>
-							</div>
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+							</div> -->
+<?php
+// WP_Query arguments
+$args = array (
+	'post_type'              => array( 'video' ),
+);
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-								<?php the_post_thumbnail( 'single-post-thumbnail', array('class' => 'home-intro-img')); ?>
-									
-									<header class="article-header">
-	
-										<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
-										
-									</header> <!-- end article header -->
-									
-								<section class="entry-content clearfix" itemprop="articleBody">
-									
-								<ul>
-									<?php $recent = new WP_Query("cat=69"); while($recent->have_posts()) : $recent->the_post();?>
-										<li class="cpt-li">
-											<iframe width="300" height="200" src="http://www.youtube.com/embed/<?php the_field('video_id'); ?>?rel=0" frameborder="0" allowfullscreen></iframe>
-											<h2><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-											<?php the_content(); ?>
-										</li>
-									<?php endwhile; ?>
-								</ul>
-									
-									<?php if(get_field('videos')): ?>
- 
-										<ul>
-									 
-										<?php while(has_sub_field('videos')): ?>
-									 
-											<li class="cpt-li">
-												<iframe width="300" height="200" src="http://www.youtube.com/embed/<?php the_sub_field('video_link'); ?>?rel=0" frameborder="0" allowfullscreen></iframe>
-												<h2><?php the_sub_field('video_name'); ?></h2>
-												<p><?php the_sub_field('video_description'); ?></p>
-												
-											</li>
-									 
-										<?php endwhile; ?>
-									 
-										</ul>
-									 
-									<?php endif; ?>
-									
-							</section> <!-- end article section -->
+// The Query
+$query = new WP_Query( $args );
 
-								<footer class="article-footer">
-									<?php the_tags('<span class="tags">' . __('Tags:', 'jbbtheme') . '</span> ', ', ', ''); ?>
+// The Loop
+if ( $query->have_posts() ) {
+	while ( $query->have_posts() ) {
+		$query->the_post();
+    // do something
+		?>
 
-								</footer> <!-- end article footer -->
+		<?php the_title(); ?>
 
-								<?php comments_template(); ?>
 
-							</article> <!-- end article -->
+
+<?php	}
+} else {
+	// no posts found
+}
+
+// Restore original Post Data
+wp_reset_postdata();
+?>
 
 							
-							<?php endwhile; else : ?>
-								<pre>No Content</pre>
-							<?php endif; ?>
-
-
-						</div> <!-- end #main -->
