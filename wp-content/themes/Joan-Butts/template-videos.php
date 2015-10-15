89ce2d55ba98b3feb066b97acdd7ac6095e5ad37
hectornucv <hectornucv@gmail.com>
2015-10-16 09:03:11 +1000

    Video Styling

--- wp-content/themes/Joan-Butts/template-videos.php
+++ wp-content/themes/Joan-Butts/template-videos.php
@@ -20,17 +20,51 @@ $args = array (
 $query = new WP_Query( $args );
 
 // The Loop
-if ( $query->have_posts() ) {
+if ( $query->have_posts() ) {?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+			<?php the_post_thumbnail( 'single-post-thumbnail', array('class' => 'home-intro-img')); ?>
+				
+				<header class="article-header">
+
+					<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
+					
+				</header> <!-- end article header -->
+				<?php 
+					if(!empty(get_user_role())) {
+				?>
+				<section class="entry-content clearfix" itemprop="articleBody">
+				<ul>
+<?php
 	while ( $query->have_posts() ) {
 		$query->the_post();
     // do something
 		?>
+				<li class="cpt-li">
+					<?php the_field('video'); ?>
+					<h2><?php the_title(); ?></h2>
+					<p><?php the_content(); ?></p>
+					
+				</li>
 
-		<?php the_title(); ?>
+<?php	}?>
+		</ul>
+		</section> <!-- end article section -->
+		<?php
+			}
+			else {
+		?>
+				<p>To view the library</p><a class="btn btn-primary" href="/register/">Register Today!</a>
+		<?php
+			}
+		?>
+			<!-- <footer class="article-footer">
+				<?php the_tags('<span class="tags">' . __('Tags:', 'jbbtheme') . '</span> ', ', ', ''); ?>
 
+			</footer> --> <!-- end article footer -->
+			
+</article> <!-- end article -->
 
-
-<?php	}
+<?php
 } else {
 	// no posts found
 }
@@ -38,5 +72,3 @@ if ( $query->have_posts() ) {
 // Restore original Post Data
 wp_reset_postdata();
 ?>
-
-							
