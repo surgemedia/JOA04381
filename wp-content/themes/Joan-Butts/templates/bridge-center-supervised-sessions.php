8571e76f37f0fd19aa2613c8bb781f0a1f0b9b8a
Vani <surge@Desktop-10s-iMac.local>
2015-11-05 15:23:54 +1000

    bridge center

--- wp-content/themes/Joan-Butts/templates/bridge-center-supervised-sessions.php
+++ wp-content/themes/Joan-Butts/templates/bridge-center-supervised-sessions.php
@@ -1,4 +1,4 @@
-<ul id="holiday-list">
+
     <?php // WP_Query arguments
         $args = array (
 		'post_type'              => array( 'tribe_events' ),
@@ -18,33 +18,42 @@
 		
 		// The Loop
 		if ( $event_query->have_posts() ) {
-			while ( $event_query->have_posts() ) {
-				$event_query->the_post();  ?>
-			<?php 
-		$the_event = get_post();
-		$text_date = date("M jS, Y", strtotime($the_event->EventStartDate));
-		 ?>
-	    <?php /*if( strtotime($text_date) > strtotime('now') ) {*/ ?>
-            <li class="holiday live-lessons">
-                <?php
-                    if(get_sub_field('holiday_image')) { ?>
-                        <img class="holiday-img" src="<?php the_sub_field('holiday_image'); ?>">
-                <?php } ?>
-                <h3 class="holiday-title"><?php the_title(); ?></h3>
-                <h5 class="date-blue"><?php echo $text_date ?></h5>
-                <p>  <?php the_content(); ?></p>
-                <div class="holiday-buttons">
-                    <a class="btn view-details" href="<?php echo get_permalink(); ?>">View Details</a>
-                </div>
-            </li>
-        
-	    <?php
-	// }	
-	}
-	} else {
+			?>
+			<section class="supervised-sessions">
+				<h2>Supervised Sessions</h2>
+				<p>Ask for advice from your tutor. Each session starts with a practice hand with Joan. No need to come with a partner.</p>
+				<ul id="holiday-list">
+					<?php
+					while ( $event_query->have_posts() ) {
+						$event_query->the_post();  ?>
+						<?php 
+						$the_event = get_post();
+						$text_date = date("M jS, Y", strtotime($the_event->EventStartDate));
+						 ?>
+					    <?php /*if( strtotime($text_date) > strtotime('now') ) {*/ ?>
+				            <li class="holiday live-lessons">
+				                <?php
+				                    if(get_sub_field('holiday_image')) { ?>
+				                        <img class="holiday-img" src="<?php the_sub_field('holiday_image'); ?>">
+				                <?php } ?>
+				                <h3 class="holiday-title"><?php the_title(); ?></h3>
+				                <h5 class="date-blue"><?php echo $text_date ?></h5>
+				                <p>  <?php the_content(); ?></p>
+				                <div class="holiday-buttons">
+				                    <a class="btn view-details" href="<?php echo get_permalink(); ?>">View Details</a>
+				                </div>
+				            </li>
+				        
+					    <?php
+					// }	
+					}
+					?>
+				</ul>	
+			</section>
+			<?php
+		} else {
 			// no posts found
 		}
 	// Restore original Post Data
 		wp_reset_postdata();
 ?>
-</ul>
